818718c0b82812fd14d8be3ce424fd97c7ce2a8d
Matheus Westhelle <matheuswesthelle@hotmail.com>
2019-12-13 11:43:16 -0300

    Move logging to client side

--- client.py
+++ client.py
@@ -1,15 +1,42 @@
 import argparse
+import logging
 import socket
+import time
 
 MSG_SIZE = 1024
 
 
+def a_second_has_elapsed(current_time: float, start_time: float) -> bool:
+    """
+    Given two timestamps (unix epochs), a starting time and a current time,
+    checks if the current time is at least a second later than the starting
+    time
+    """
+    return current_time - start_time >= 1
+
+
 def connect(ip: str, port: int):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((ip, port))
+    logging.basicConfig(filename='bandwidth.log',
+                        filemode='a',
+                        format='%(asctime)s - %(message)sMBps',
+                        level=logging.INFO)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((ip, port))
         print(f"connected to {ip} on port {port}.")
+        start_time = time.time()
+        total_sent_data = 0
         while True:
-            s.send(b'X'*MSG_SIZE)
+            # sock.send returns amount of bytes successfully sent
+            sent_data = sock.send(b'x' * MSG_SIZE)
+            total_sent_data += sent_data
+            current_time = time.time()
+            if a_second_has_elapsed(current_time, start_time):
+                # bandwidth per second is data received in that second
+                # multiplied by number of bits and divided by a Megabit
+                bandwidth_in_last_second = total_sent_data * 8 / 2**20
+                logging.info(round(bandwidth_in_last_second, 2))
+                start_time = time.time()
+                total_sent_data = 0
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -1,7 +1,5 @@
 import argparse
-import logging
 import socket
-import time
 
 BUFFER_SIZE = 2**10
 
@@ -22,27 +20,14 @@ def listen(port: int):
     """
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        logging.basicConfig(filename='bandwidth.log', filemode='a',
-                            format='%(asctime)s - %(message)sMBps', level=logging.INFO)
         sock.bind(('', port))
         sock.listen(0)
         print(f"Listening on port {port}...")
         conn, addr = sock.accept()
         print(f"Accepted connection to {addr}")
-        start_time = time.time()
-        received_data = 0
         with conn:
             while True:
                 data = conn.recv(BUFFER_SIZE)
-                received_data += len(data)
-                current_time = time.time()
-                if a_second_has_elapsed(current_time, start_time):
-                    # bandwidth per second is data received in that second
-                    # multiplied by number of bits and divided by a Megabit
-                    bandwidth_in_last_second = received_data * 8 / 2**20
-                    logging.info(round(bandwidth_in_last_second, 2))
-                    start_time = time.time()
-                    received_data = 0
                 if not data:
                     return
 
